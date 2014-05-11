408f2ae373f17c625377844a5657980c633dde32
teyong <enriqueliu125@gmail.com>
2014-05-10 23:51:35 -0700

    added files and plots

---
+++ plot1.R
@@ -0,0 +1,19 @@
+setwd("/Users/teyongliuwong/Desktop/John Hopkins University Data Science Specialization/4 Exploratory Data Analysis/Peer Assignment 1")
+download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip", destfile="household_power_consumption.zip", method="curl")
+unzip("household_power_consumption.zip", "household_power_consumption.txt")
+
+data <- read.table("household_power_consumption.txt", sep = ";", header=TRUE)
+data <- data[data$Date=="1/2/2007"|data$Date=="2/2/2007", ]
+data$Date <- strptime(data$Date, format="%d/%m/%Y")
+data$Global_active_power <- as.numeric(as.character(data$Global_active_power))
+data$Global_reactive_power <- as.numeric(as.character(data$Global_reactive_power))
+data$Voltage <- as.numeric(as.character(data$Voltage))
+data$Sub_metering_1 <- as.numeric(as.character(data$Sub_metering_1))
+data$Sub_metering_2 <- as.numeric(as.character(data$Sub_metering_2))
+data$Sub_metering_3 <- as.numeric(as.character(data$Sub_metering_3))
+rownames(data) <- NULL
+
+png(file="plot1.png")
+with(data, hist(Global_active_power, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)"))
+dev.off()
+
